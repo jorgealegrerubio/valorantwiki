576834ee92ed929019609484c91ddacbfcaf4346
Jorge Alegre Rubio <jorgealegre@rudo.es>
2024-05-20 13:23:25 +0200

    Add clean arquitecture structure and splash view

---
+++ valorantwiki/valorantwiki/Presentation/App/app.swift
@@ -0,0 +1,18 @@
+//
+//  valorantwikiApp.swift
+//  valorantwiki
+//
+//  Created by Jorge on 20/5/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct valorantwikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
